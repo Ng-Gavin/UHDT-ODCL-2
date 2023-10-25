df84df8a151d164f342d231b52dc59f53fc9c9da
Tonyl73 <124114090+Tonyl73@users.noreply.github.com>
2023-10-24 20:38:32 -1000

    Update pytesseract_test_main.py
    created loop to test the pytess functions

--- pytesseract_test_main.py
+++ pytesseract_test_main.py
@@ -1,30 +1,30 @@
 from PIL import Image
 from pathlib import Path
+from AlphaNumbericFucntionV2 import image2textConf
+from time import sleep
 import pytesseract
 import cv2
 
-#get the path/directory
-folder_dir = 'test_pngs'
+#get the path/directory; replace name in quotes with file/directory name
+folder_dir = 'UHDTr1-ground-truth'
 #from website https://www.geeksforgeeks.org/how-to-iterate-through-images-in-a-folder-python/
 #consider https://www.geeksforgeeks.org/python-ocr-on-all-the-images-present-in-a-folder-simultaneously/
 
-#find way to replace this array with an actual way to iterate through files.
-#temp_array = ['a_test_3_blur_20_r12.jpg','a_Test.png','a_Test_2.png','a_test_3.jpg','Upper_Ariel_Q.jpg','B_test_1.png','R_real_test_1.png','4_test_2.png']
+alphnums = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+#This creates array to hold images. replace .jpg with whatever format neccesary (will need to find way to address all types of images)
+images = [str(x) for x in Path(folder_dir).glob('*.png')]
+totalpics = len(images)
 
-images = Path(folder_dir).glob('*.png')
-#function to read words off image (we need to try and get individual letters)
+tallycorrect = 0
 for file in images:
-    img = str(file)
-    def read_img(img):
-        #custom_config = r' --oem 10, --psm 6'
-        custom_config = r'--psm 10 --oem 3'
-        print(pytesseract.image_to_string(Image.open(img),config=custom_config))
-        #loop through a bunch of images (preferably font images)
-    read_img(img)
-
-#img= 'a_test_3.jpg'
-
-
+    #cut off the folder path with [20:]
+    img = (str(file))
+    if (img[20] in alphnums):
+        print(image2textConf(img),img[20])
+        if image2textConf(img)[0] == img[20]:
+            #print("FINALLY")
+            tallycorrect+=1
+    sleep(0.5)
+#general accuracy of entire set of images
+print(f"The amount correct: {tallycorrect}, out of {totalpics}. An accuracy of {(tallycorrect/totalpics)*100:.2f}%")
     
-
-#create way to compare output with actual expected letter.
