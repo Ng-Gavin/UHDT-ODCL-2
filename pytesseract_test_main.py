7cd7783e7e815c30ea28000fd1c4f27c4b86caf8
Tonyl73 <124114090+Tonyl73@users.noreply.github.com>
2023-10-24 22:30:03 -1000

    Update pytesseract_test_main.py
    changed the slicing numbers in the comparison section

--- pytesseract_test_main.py
+++ pytesseract_test_main.py
@@ -6,7 +6,7 @@ import pytesseract
 import cv2
 
 #get the path/directory; replace name in quotes with file/directory name
-folder_dir = 'UHDTr1-ground-truth'
+folder_dir = 'sans_export'
 #from website https://www.geeksforgeeks.org/how-to-iterate-through-images-in-a-folder-python/
 #consider https://www.geeksforgeeks.org/python-ocr-on-all-the-images-present-in-a-folder-simultaneously/
 
@@ -14,17 +14,17 @@ alphnums = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 #This creates array to hold images. replace .jpg with whatever format neccesary (will need to find way to address all types of images)
 images = [str(x) for x in Path(folder_dir).glob('*.png')]
 totalpics = len(images)
-
 tallycorrect = 0
+timeslooped = 0
+
 for file in images:
-    #cut off the folder path with [20:]
+    #cut off the folder path with [12:]; have to change the number depending on file names
     img = (str(file))
-    if (img[20] in alphnums):
-        print(image2textConf(img),img[20])
-        if image2textConf(img)[0] == img[20]:
+    if (img[12] in alphnums):
+        print(image2textConf(img),img[12:],timeslooped)
+        if image2textConf(img)[0] == img[12]:
             #print("FINALLY")
             tallycorrect+=1
-    sleep(0.5)
-#general accuracy of entire set of images
+    timeslooped+=1
+    #sleep(0.5)
 print(f"The amount correct: {tallycorrect}, out of {totalpics}. An accuracy of {(tallycorrect/totalpics)*100:.2f}%")
-    
