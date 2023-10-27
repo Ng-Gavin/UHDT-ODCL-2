9069d4337beda849058dea5045ec038b92ccb461
Tonyl73 <124114090+Tonyl73@users.noreply.github.com>
2023-10-27 00:54:37 -1000

    Increased capabilities of testing script
    Can output the incorrect readings and their frequencies.
    Can output the individual accuracies for each letter (still need to be adjusted to make a variable denominator).

--- pytesseract_test_main.py
+++ pytesseract_test_main.py
@@ -1,30 +1,57 @@
 from PIL import Image
 from pathlib import Path
 from AlphaNumbericFucntionV2 import image2textConf
-from time import sleep
 import pytesseract
 import cv2
 
 #get the path/directory; replace name in quotes with file/directory name
 folder_dir = 'sans_export'
-#from website https://www.geeksforgeeks.org/how-to-iterate-through-images-in-a-folder-python/
-#consider https://www.geeksforgeeks.org/python-ocr-on-all-the-images-present-in-a-folder-simultaneously/
 
 alphnums = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 #This creates array to hold images. replace .jpg with whatever format neccesary (will need to find way to address all types of images)
 images = [str(x) for x in Path(folder_dir).glob('*.png')]
-totalpics = len(images)
-tallycorrect = 0
-timeslooped = 0
+numimages = len(images)
+#variable to keep track of loop
+timeslooped = 1
+#create dictionary to hold the incorrect and correct outputs (equivalent of hashmap?)
+CorOutputFreq= {}
+IncOutputFreq = {}
+letterFreq = {}
 
 for file in images:
-    #cut off the folder path with [12:]; have to change the number depending on file names
+    #cut off the folder path with [12:]; the number (i.e. 12) depends on file names
     img = (str(file))
+    #see if the labeled image is actually a test image (in this case the test images were all labeled for this algorithm)
     if (img[12] in alphnums):
         print(image2textConf(img),img[12:],timeslooped)
+        #add the count of correct readings to each alphanumeric to a frequency map
         if image2textConf(img)[0] == img[12]:
-            #print("FINALLY")
-            tallycorrect+=1
+            #if it already exists in the dictionary, add one
+            if image2textConf(img)[0] in CorOutputFreq:
+                CorOutputFreq[image2textConf(img)[0]] += 1
+            #if it doesn't exist, add it to the dictionary and set it to 1
+            else:
+                CorOutputFreq[image2textConf(img)[0]] = 1
+        #add the incorrect outputs (wrong char, number of times)
+        else:
+            #if incorrect value is already in there, add 1
+            if image2textConf(img)[0] in IncOutputFreq:
+                IncOutputFreq[image2textConf(img)[0]][1] += 1
+            #if it doesn't exist in dictionary yet, add it there
+            else:
+                IncOutputFreq[image2textConf(img)[0]] = [img[12],1]
+    #for testing purposes, in case you just need to see if a part of the code works without having to loop 12k times
+    if timeslooped == 500:
+        break
+
     timeslooped+=1
-    #sleep(0.5)
-print(f"The amount correct: {tallycorrect}, out of {totalpics}. An accuracy of {(tallycorrect/totalpics)*100:.2f}%")
+print(CorOutputFreq)
+print(IncOutputFreq)
+#to get overall accuracy:
+numcorrect = [int(CorOutputFreq[num]) for num in CorOutputFreq]
+totalnumcorrect = sum(numcorrect)
+print(f'Overall Accuracy: {sum(numcorrect)/len(images)}')
+#to get individual accuracy (need to change code for 361. It wont always be 361)
+for char in CorOutputFreq:
+    print(f'Accuracy for "{char}" is {(CorOutputFreq[char]/361)*100:.2f}%')
+    
